89400704ad0af76db2a23dc7b5795c9bb877cd63
Jubril Oyetunji <cyberdev01@protonmail.com>
2022-01-05 16:29:43 +0100

    init v2 webserver

---
+++ webapp-v2/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+
+func main(){
+	l := log.New(os.Stdout,"[ webapp-v2 ]",log.LstdFlags)
+	r := http.NewServeMux()
+	r.HandleFunc("/",func(rw http.ResponseWriter, r *http.Request) {
+		http.ServeFile(rw,r,"./static/index.html")		
+	})
+	l.Printf("Starting server on port 9090...")	
+	http.ListenAndServe(":9090",r)
+}
