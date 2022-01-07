6d98810386c5574e7598e7441e34ac0a98130c86
Jubril Oyetunji <cyberdev01@protonmail.com>
2022-01-07 08:29:00 +0100

    log req path

--- webapp-v1/main.go
+++ webapp-v1/main.go
@@ -6,14 +6,13 @@ import (
 	"os"
 )
 
-
-func main(){
-	l := log.New(os.Stdout,"[ webapp-v1 ]",log.LstdFlags)
+func main() {
+	l := log.New(os.Stdout, "[ webapp-v1 ]", log.LstdFlags)
 	r := http.NewServeMux()
-	r.HandleFunc("/home",func(rw http.ResponseWriter, r *http.Request) {
-		l.Printf("request recived from %s",r.Host)
-		http.ServeFile(rw,r,"./static/index.html")		
+	r.HandleFunc("/home", func(rw http.ResponseWriter, r *http.Request) {
+		l.Printf("request recived from %s on %s", r.Host, r.URL.Path)
+		http.ServeFile(rw, r, "./static/index.html")
 	})
-	l.Printf("Starting server on port 8080")	
-	http.ListenAndServe(":8080",r)
+	l.Printf("Starting server on port 8080")
+	http.ListenAndServe(":8080", r)
 }
