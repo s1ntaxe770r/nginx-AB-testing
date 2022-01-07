94ccc5cdbe666988e0b2beca75f44712dae2784b
Jubril Oyetunji <cyberdev01@protonmail.com>
2022-01-07 15:33:06 +0100

    add version handler

--- webapp-v1/main.go
+++ webapp-v1/main.go
@@ -13,6 +13,9 @@ func main() {
 		l.Printf("request recived from %s on %s", r.Host, r.URL.Path)
 		http.ServeFile(rw, r, "./static/index.html")
 	})
+	r.HandleFunc("/version", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("v1"))
+	})
 	l.Printf("Starting server on port 8080")
 	http.ListenAndServe(":8080", r)
 }
--- webapp-v2/main.go
+++ webapp-v2/main.go
@@ -6,13 +6,15 @@ import (
 	"os"
 )
 
-
-func main(){
-	l := log.New(os.Stdout,"[ webapp-v2 ]",log.LstdFlags)
+func main() {
+	l := log.New(os.Stdout, "[ webapp-v2 ]", log.LstdFlags)
 	r := http.NewServeMux()
-	r.HandleFunc("/",func(rw http.ResponseWriter, r *http.Request) {
-		http.ServeFile(rw,r,"./static/index.html")		
+	r.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		http.ServeFile(rw, r, "./static/index.html")
 	})
-	l.Printf("Starting server on port 9090...")	
-	http.ListenAndServe(":9090",r)
+	r.HandleFunc("/version", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("v2"))
+	})
+	l.Printf("Starting server on port 9090...")
+	http.ListenAndServe(":9090", r)
 }
