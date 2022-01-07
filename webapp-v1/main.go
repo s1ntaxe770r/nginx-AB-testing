bc58599a49ba4eeca238371e29855f54aef4d452
Jubril Oyetunji <cyberdev01@protonmail.com>
2022-01-07 08:14:00 +0100

    add smol logging and change path

--- webapp-v1/main.go
+++ webapp-v1/main.go
@@ -10,7 +10,8 @@ import (
 func main(){
 	l := log.New(os.Stdout,"[ webapp-v1 ]",log.LstdFlags)
 	r := http.NewServeMux()
-	r.HandleFunc("/",func(rw http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/home",func(rw http.ResponseWriter, r *http.Request) {
+		l.Printf("request recived from %s",r.Host)
 		http.ServeFile(rw,r,"./static/index.html")		
 	})
 	l.Printf("Starting server on port 8080")	
